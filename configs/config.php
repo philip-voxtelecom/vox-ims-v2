03c1bb1ab858e47eddde35bcdb78dc28fafc2a3e
Philip Correia <philipc@voxtelecom.co.za>
2012-06-27 13:28:34 +0200

    Working Version 0.9.1

--- configs/config.php
+++ configs/config.php
@@ -27,13 +27,17 @@ $config->adsl_dbuser = 'iprisedevel';
 $config->adsl_dbname = 'radius-devel';
 $config->adsl_dbpass = 'develiprise';
 
-$config->adsl_meta_dbtype = 'mysql';
+$config->adsl_meta_dbtype = 'mysqli';
 $config->adsl_meta_dbhost = 'localhost';
 $config->adsl_meta_dbuser = 'adslmeta';
 $config->adsl_meta_dbname = 'adslmeta';
 $config->adsl_meta_dbpass = 'y6t5r4e3';
 
 $config->use_cache = TRUE;
+// mysqli or pdodb
+// currently there is a problem with pdodb
+// This is un used
+// $config->cachetype = 'mysqli';
 
 
 if (is_readable($GLOBALS['documentroot'] . '/configs/local_config.php')) {
