16c0c4e1fa5509d79c01065a8c842e0a3724ae71
Philip Correia <philipc@voxtelecom.co.za>
2012-06-28 13:15:29 +0200

    created methods

--- kernel/adsl/_owner_controller.php
+++ kernel/adsl/_owner_controller.php
@@ -7,17 +7,77 @@ require_once('_owner_model.php');
 require_once('_owner_view.php');
 
 class OwnerController {
-    public function create() {
-        
+
+    protected $owner;
+    private $required_create_params = array(
+        'login' => array(
+            'mandatory' => TRUE,
+            'validation' => ''
+        ),
+        'password' => array(
+            'mandatory' => TRUE,
+            'validation' => ''
+        ),
+    );
+
+    public function create($parameters) {
+        try {
+            validateParams($this->required_create_params, $parameters);
+        } catch (InvalidArgumentException $e) {
+            throw new Exception("Parameter " . $e->getMessage() . " required");
+        }
+        $this->owner = OwnerFactory::Create();
+
+        foreach ($parameters as $parameter => $value) {
+            $this->owner->$parameter = $value;
+        }
+
+        try {
+            $this->owner->create();
+        } catch (Exception $e) {
+            throw new Exception("Error creating owner: " . $e->getMessage());
+        }
+        return $this->owner->id();
     }
-    
-    public function update() {
-        
+
+    public function read($id) {
+        if (empty($id))
+            throw new InvalidArgumentException("Invalid argument");
+
+        if (empty($this->owner))
+            $this->owner = OwnerFactory::Create();
+        return $this->owner->read($id);
     }
-    
-    public function delete() {
-        
+
+    public function update($parameters) {
+        if (empty($this->owner))
+            throw new Exception("No owner loaded for update");
+        //todo check parameters
+        $this->owner->update($parameters);
     }
+
+    public function delete($id = NULL) {
+        if (empty($id) and empty($this->owner))
+            throw new InvalidArgumentException("Invalid argument");
+
+        if (empty($this->owner)) {
+            try {
+                $this->owner = OwnerFactory::Create();
+                $this->owner->read($id);
+            } catch (Exception $e) {
+                throw new Exception("Could not load owner for delete: " . $e->getMessage());
+            }
+        }
+
+        try {
+            $this->owner->delete();
+            unset($this->owner);
+        } catch (Exception $e) {
+            throw new Exception("Problem encountered deleting account");
+        }
+        return TRUE;
+    }
+
 }
 
 ?>
