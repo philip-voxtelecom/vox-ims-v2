b9b92529fe6147aafa52561b04ab5152d65e5ddd
Philip Correia <philipc@voxtelecom.co.za>
2012-06-29 15:27:53 +0200

    Cleaned up

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 644 {} \;
-mkdir templates_c cache
+mkdir templates_c cache &> /dev/null
 chmod 774 templates_c cache
 chgrp apache -R *
 
