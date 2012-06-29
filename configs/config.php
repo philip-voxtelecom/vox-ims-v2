00b6c2a0cddc3058fd729e76c04091dc1f72dd7a
Philip Correia <philipc@voxtelecom.co.za>
2012-06-29 13:50:16 +0200

    cleaned up

--- configs/config.php
+++ configs/config.php
@@ -20,18 +20,11 @@ $config->documentroot = $GLOBALS['documentroot'];
 // set display row limit for tables
 $config->displayRowLimit = 20;
 
-//ADSL management database details
-$config->adsl_dbtype = 'pg';
-$config->adsl_dbhost = 'localhost';
-$config->adsl_dbuser = 'iprisedevel';
-$config->adsl_dbname = 'radius-devel';
-$config->adsl_dbpass = 'develiprise';
-
-$config->adsl_meta_dbtype = 'mysqli';
-$config->adsl_meta_dbhost = 'localhost';
-$config->adsl_meta_dbuser = 'adslmeta';
-$config->adsl_meta_dbname = 'adslmeta';
-$config->adsl_meta_dbpass = 'y6t5r4e3';
+//$config->adsl_meta_dbtype = 'mysqli';
+//$config->adsl_meta_dbhost = 'localhost';
+//$config->adsl_meta_dbuser = 'adslmeta';
+//$config->adsl_meta_dbname = 'adslmeta';
+//$config->adsl_meta_dbpass = 'y6t5r4e3';
 
 $config->use_cache = TRUE;
 // mysqli or pdodb
