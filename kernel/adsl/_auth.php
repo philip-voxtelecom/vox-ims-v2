5ed085970790913183d9d60661830a67906fa535
Philip Correia <philipc@voxtelecom.co.za>
2012-06-29 15:24:18 +0200

    loadMap gets info out of database now

--- kernel/adsl/_auth.php
+++ kernel/adsl/_auth.php
@@ -6,9 +6,11 @@ class accountAuth {
     private $admin = false;
     private $loginId;
     private $userMap = array();
+    private $dbh;
 
     public function __construct($user) {
-        $this->loadUserMap();
+        $this->dbh = MetaDatabaseConnection::get('accountauth')->handle();
+        $this->loadUserMap($user);
         $effectiveUser = $this->getEffectiveLogin($user);
         $this->setEffectiveLogin($effectiveUser);
     }
@@ -33,24 +35,32 @@ class accountAuth {
         if (isset($this->userMap[$user])) {
             return $this->userMap[$user];
         } else {
-            return $user;
+            throw new Exception("No user map exists for $user");
         }
     }
 
     /* this is the user on the ADSL system */
+
     public function setEffectiveLogin($user) {
-        if ($user == 'interprise') {
-            $this->setLoginId('Datapro');
-            //$this->setAdmin();
-        } elseif ($user == 'iprise-demo') {
-            $this->setLoginId('c6e5de8b-a0e7-480b-8c6c-eec43ecbe23e');
-        }
+        $this->setLoginId($user);
+        //$this->setAdmin();
     }
 
     /* map the logged in user to an ADSL system user */
-    protected function loadUserMap() {
-        $this->userMap['philip'] = 'interprise';
-        $this->userMap['demo'] = 'iprise-demo';
+
+    protected function loadUserMap($user) {
+        $query = "select * from usermap where loginuser='$user'";
+        if ($GLOBALS['config']->adsl_meta_dbtype == 'mysqli') {
+            $result = $this->dbh->query($query);
+            $row = $result->fetch_assoc();
+        } else {
+            throw new Exception("Unsupported DB type for adsl_meta_dbtype");
+        }
+        if (!empty($row)) {
+            $this->userMap[$row['loginuser']] = $row['mappeduser'];
+        } else {
+            throw new Exception("No ADSL user map found for $user");
+        }
     }
 
 }
@@ -58,5 +68,4 @@ class accountAuth {
 //$login = new accountAuth($_SERVER['PHP_AUTH_USER']);
 // TODO is this right?
 $login = new accountAuth($_SESSION['name']);
-
 ?>
